e701a79b0440fc034ef3dd9e8976b4312d8de368
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-06-02 19:55:28 +0200

    feat add user interaction for creating a new product

--- src/lessons/java/shop/Main.java
+++ src/lessons/java/shop/Main.java
@@ -1,6 +1,7 @@
 package lessons.java.shop;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,21 +16,80 @@ public class Main {
         Cuffie OnTrack = new Cuffie("OnTrack", "DYSON", new BigDecimal(500.00), true, "Gold");
 
         // Stampa prodotti
-        System.out.println("ELENCO DEI PRODOTTI \n");
-        System.out.println("--------------------");
-        System.out.println("SMARTPHONE: \n \n");
-        System.out.println(S24ultra.toString() + "\n");
-        System.out.println("--------------------");
-        System.out.println("TELEVISORI: \n \n");
-        System.out.println(Bravia.toString() + "\n");
-        System.out.println(FourKLG.toString() + "\n");
-        System.out.println(Funk.toString() + "\n");
-        System.out.println("--------------------");
-        System.out.println("CUFFIE: \n \n");
-        System.out.println(WJBL.toString() + "\n");
-        System.out.println(Major.toString() + "\n");
-        System.out.println(OnTrack.toString() + "\n");
+        // System.out.println("ELENCO DEI PRODOTTI \n");
+        // System.out.println("--------------------");
+        // System.out.println("SMARTPHONE: \n \n");
+        // System.out.println(S24ultra.toString() + "\n");
+        // System.out.println("--------------------");
+        // System.out.println("TELEVISORI: \n \n");
+        // System.out.println(Bravia.toString() + "\n");
+        // System.out.println(FourKLG.toString() + "\n");
+        // System.out.println(Funk.toString() + "\n");
+        // System.out.println("--------------------");
+        // System.out.println("CUFFIE: \n \n");
+        // System.out.println(WJBL.toString() + "\n");
+        // System.out.println(Major.toString() + "\n");
+        // System.out.println(OnTrack.toString() + "\n");
 
+        // facciamo inserire all'utente un nuovo prodotto
+        System.out.println("Inserisci un nuovo prodotto: ");
+        // chiediamo che tipo di prodotto vuole inserire
+        System.out.println("Che tipo di prodotto vuoi inserire? Smartphone, Televisore o Cuffie?");
+        Scanner scanner = new Scanner(System.in);
+        String typeOfProduct = scanner.nextLine().trim().toLowerCase();
+        // creiamo il tipo di prodotto richiesto
+        if (typeOfProduct.equals("smartphone")) {
+            System.out.println("Inserisci il nome dello smartphone: ");
+            String name = scanner.nextLine().trim();
+            System.out.println("Inserisci la marca dello smartphone:");
+            String brand = scanner.nextLine().trim();
+            System.out.println("inserisci il prezzo dello smartphone;");
+            BigDecimal price = scanner.nextBigDecimal();
+            System.out.println("inserisci il codice IMEI dello smartphone:");
+            String imei = scanner.nextLine();
+            System.out.println("Inserisci la memoria dello smartphone in GB: ");
+            int memory = scanner.nextInt();
+            // creiamo l'oggetto smarthone
+            Smartphone newSmartphone = new Smartphone(name, brand, price, imei, memory);
+            // stampiamo la scheda dello smartphone appena creato
+            System.out.println("Scheda del nuovo smartphone: \n");
+            System.out.println(newSmartphone.toString());
+        } else if (typeOfProduct.equals("televisore")) {
+            System.out.println("Inserisci il nome dello Televisore: ");
+            String name = scanner.nextLine().trim();
+            System.out.println("Inserisci la marca dello Televisore:");
+            String brand = scanner.nextLine().trim();
+            System.out.println("inserisci il prezzo dello Televisore;");
+            BigDecimal price = scanner.nextBigDecimal();
+            System.out.println("Questo Televisore è Smart? (true/false)");
+            boolean isSmart = scanner.nextBoolean();
+            System.out.println("Inserisci la grandezza dello schermo del Televisore in pollici: ");
+            int inches = scanner.nextInt();
+            // creiamo l'oggetto smarthone
+            Televisore newTelevisore = new Televisore(name, brand, price, isSmart, inches);
+            // stampiamo la scheda dello Televisore appena creato
+            System.out.println("Scheda del nuovo Televisore: \n");
+            System.out.println(newTelevisore.toString());
+        } else if (typeOfProduct.equals("cuffie")) {
+            System.out.println("Inserisci il nome dello Cuffie: ");
+            String name = scanner.nextLine().trim();
+            System.out.println("Inserisci la marca dello Cuffie:");
+            String brand = scanner.nextLine().trim();
+            System.out.println("inserisci il prezzo dello Cuffie;");
+            BigDecimal price = scanner.nextBigDecimal();
+            System.out.println("Queste Cuffie sono wireless? (true/false)");
+            boolean isWireless = scanner.nextBoolean();
+            System.out.println("Inserisci il colore delle cuffie: ");
+            String color = scanner.nextLine();
+            // creiamo l'oggetto smarthone
+            Cuffie newCuffie = new Cuffie(name, brand, price, isWireless, color);
+            // stampiamo la scheda dello Cuffie appena creato
+            System.out.println("Scheda delle nuove Cuffie: \n");
+            System.out.println(newCuffie.toString());
+        } else {
+            System.out.println("Tipo di prodotto non riconosciuto. Errore nell'inserimento.");
+        }
+        scanner.close();
     }
 
 }
